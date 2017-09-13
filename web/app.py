84539c861f7c0e2a51d8d5f62bf0dbabb13c2d84
Steve Recio <stevenrecio@gmail.com>
2017-09-12 20:12:06 -0400

    render from template

--- web/app.py
+++ web/app.py
@@ -1,17 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-html = """
-<html>
-<body>
-<img src="http://i.imgur.com/jrtNP6s.jpg" width="600px; border-bottom: solid 1px black; padding-bottom: 10px; margin-bottom: 10px; clear: right;" />
-</body>
-</html>
-"""
 
 @app.route('/')
 def moe():
-    return html
+    return render_template('moe.html')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
